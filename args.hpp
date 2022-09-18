4e0743dfc482fee142a9c3ece2195f2ce8d94be3
Dimitry Ishenko <dimitry.ishenko@gmail.com>
2022-09-18 01:37:47 -0400

    Update copyright years

--- args.hpp
+++ args.hpp
@@ -1,5 +1,5 @@
 ////////////////////////////////////////////////////////////////////////////////
-// Copyright (c) 2018-2021 Dimitry Ishenko
+// Copyright (c) 2018-2022 Dimitry Ishenko
 // Contact: dimitry (dot) ishenko (at) (gee) mail (dot) com
 //
 // Distributed under the GNU GPL license. See the LICENSE.md file for details.
