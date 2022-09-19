0d07a8b1d142d1cd0ac6919e909e2e6582bc7ad8
Dimitry Ishenko <dimitry.ishenko@gmail.com>
2022-09-19 01:27:10 -0400

    example1: update

--- example/example1.cpp
+++ example/example1.cpp
@@ -1,31 +1,28 @@
 #include <iostream>
-
-// 1. Include the header.
 #include "pgm/args.hpp"
 
 int main(int argc, char* argv[])
 {
-    // 2. Define options and positional parameters.
     pgm::args args
     {
         { "-v", "--version", "Show version and exit." },
         { "-h", "--help",    "Show this help screen and exit." },
     };
 
-    // 3. Parse command line arguments.
     args.parse(argc, argv);
 
-    // 4. Examine options and positional parameters.
-    if(args["--help"])
-        std::cout << args.usage(argv[0]) << std::endl;
-
-    else if(args["--version"])
-        std::cout << argv[0] << " 0.42" << std::endl;
-
-    else
+    if(args["--version"])
     {
-        std::cout << "Hello world!" << std::endl;
-        std::cout << "Use -h or --help to see the help screen." << std::endl;
+        std::cout << argv[0] << " 0.42" << std::endl;
+        return 0;
     }
+    if(args["--help"])
+    {
+        std::cout << args.usage(argv[0]) << std::endl;
+        return 0;
+    }
+
+    std::cout << "Hello world!" << std::endl;
+    std::cout << "Use -h or --help to see the help screen." << std::endl;
     return 0;
 }
